e8bf0409ce796651165061c4a89b2a095c53d90a
Stjoo0925 <stjoo0925@gmail.com>
2024-08-03 13:36:20 +0900

    first commit

---
+++ login_spring/src/test/java/com/stjoo0925/login_spring/LoginSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stjoo0925.login_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
